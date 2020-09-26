39842363aea6231ef10a491b1909cb8248823dcd
tfe <tfecreative@gmail.com>
2020-09-26 14:54:58 -0400

    Update app and home styles
    Changed spacing and backgrounds

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -1,5 +1,10 @@
 import React from "react";
+import "./Home.scss";
 
 export const Home = () => {
-  return <section className="section section-home">hi</section>;
+  return (
+    <section className="section home">
+      <div>hi</div>
+    </section>
+  );
 };
