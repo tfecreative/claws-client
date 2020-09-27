614d41ad6d0c8004aa5d47252d290d97e8775874
tfe <tfecreative@gmail.com>
2020-09-26 20:43:09 -0400

    Add home hero
    - Content for main panel

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -1,10 +1,11 @@
 import React from "react";
+import { HomeHero } from './Home.Hero';
 import "./Home.scss";
 
 export const Home = () => {
   return (
-    <section className="section home">
-      <div>hi</div>
+    <section className="section home bg-secondary">
+      <HomeHero></HomeHero>
     </section>
   );
 };
